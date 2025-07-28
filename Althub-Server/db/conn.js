3c61a355bdb52dfd2a7c92976ab7b21662cbe045
Meet Gandhi <103819209+meet4041@users.noreply.github.com>
2025-07-28 22:25:13 +0530

    Update conn.js

--- Althub-Server/db/conn.js
+++ Althub-Server/db/conn.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectToMongo = async () => {
     try {
-        await mongoose.connect("mongodb+srv://202412020:3VaxJWTs1VHhJe6X@cluster0.bxshd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
+        await mongoose.connect("-----your database api-----");
         console.log("Connected To Althub !!!");
     } catch (error) {
         console.log("Error in connection : ", error);
